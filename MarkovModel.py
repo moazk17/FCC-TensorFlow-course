768c26363d41bf72c77ba1bbf538ee8726bbb3c6
Moazk127 <mkgmahra@uwaterloo.ca>
2023-05-03 15:41:41 -0400

    MarkovModel progress

---
+++ MarkovModel.py
@@ -0,0 +1,10 @@
+import tensorflow_probability as tfp
+import tensorflow as tf
+
+tfd = tfp.distributions
+initial_distribution = tfd.Categorical(probs=[0.8, 0.2]) # The first day in our sequence has an 80% chance of being cold.
+# A cold day has a 30% chance of being followed by a hot day. A hot day has a 20% chance of being followed by a cold day.
+transition_distribution = tfd.Categorical(probs=[[0.7, 0.3], [0.2, 0.8]]) 
+observation_distribution = tfd.Normal(loc=[0., 15.], scale=[5., 10.]) # On each day the temperature is normally distributed with mean and standard deviation 0 and 5 on a cold day and mean and standard deviation 15 and 10 on a hot day.
+                                                 
+                                                 
